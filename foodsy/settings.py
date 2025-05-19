9f9b2ccf1788558d69b656aaa5208f041ea039d2
Milan <surkos.milan@gmail.com>
2025-05-19 20:45:01 +0200

    configurate settings, databaze , requirements subor

--- foodsy/settings.py
+++ foodsy/settings.py
@@ -74,8 +74,15 @@ WSGI_APPLICATION = 'foodsy.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'foodsy',
+        'USER': 'root',
+        'PASSWORD': 'heslojeveslo',
+        'HOST': 'localhost',  # alebo IP adresa databázového servera
+        'PORT': '3306',       # predvolený port pre MySQL
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
 
