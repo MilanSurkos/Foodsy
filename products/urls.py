269a19afccf7b96323b1eb317623610d92ebe2f7
git config --global user.name "Iveta Szaboova <git config --global user.email "ivetka.szaboova@gmail.com>
2025-05-26 18:02:17 +0200

    rozdelenie produktov podla kategrorii a podkategorii na samostatne stranky

--- products/urls.py
+++ products/urls.py
@@ -4,8 +4,6 @@ from . import views
 app_name = 'products'
 
 urlpatterns = [
-    path('', views.product_list, name='product_list'),
     path('kategorie/', views.kategorie_list, name='kategorie_list'),
-    path('kategorie/<int:kategorie_id>/', views.produkty_podla_kategorie, name='produkty_podla_kategorie'),
-    path('kategoria/<int:subcategory_id>/', views.produkty_podla_kategorie, name='produkty_podla_kategorie'),
+    path('kategoria/<int:category_id>/', views.produkty_podla_kategorie, name='produkty_podla_kategorie'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,33 +1,72 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Product, Kategorie
-from django.core.paginator import Paginator
-
-
-def index(request):
-    products = Product.objects.all()
-    return render(request, 'products/base.html', {'products': products})
-
-def product_list(request):
-    product_list = Product.objects.all()
-    paginator = Paginator(product_list, 10)  # 10 per page
-    page = request.GET.get('page')
-    products = paginator.get_page(page)
-    return render(request, 'products/product_list.html', {'products': products})
+from django.shortcuts import render
 
 def kategorie_list(request):
-    kategorie = Kategorie.objects.filter(parent__isnull=True)
+    jedlo_sub = [
+        {
+            'name': 'Čerstvé potraviny',
+            'subsubcategories': [
+                {'id': 10, 'name': 'Ovocie'},
+                {'id': 11, 'name': 'Zelenina'},
+                {'id': 12, 'name': 'Mliečne výrobky'},
+            ]
+        },
+        {
+            'name': 'Trvanlivé potraviny',
+            'subsubcategories': [
+                {'id': 13, 'name': 'Konzervy'},
+                {'id': 14, 'name': 'Sušené potraviny'},
+                {'id': 15, 'name': 'Cestoviny a ryža'},
+            ]
+        },
+        {
+            'name': 'Mrazené potraviny',
+            'subsubcategories': [
+                {'id': 16, 'name': 'Mrazená zelenina'},
+                {'id': 17, 'name': 'Mrazené ovocie'},
+                {'id': 18, 'name': 'Mrazené mäsové výrobky'},
+            ]
+        }
+    ]
 
-    return render(request, 'products/kategorie_list.html', {'kategorie': kategorie})
+    pitie_sub = [
+        {
+            'name': 'Nealkoholické nápoje',
+            'subsubcategories': [{'id': 4, 'name': 'Nealkoholické nápoje'}]
+        },
+        {
+            'name': 'Alkoholické nápoje',
+            'subsubcategories': [{'id': 5, 'name': 'Alkoholické nápoje'}]
+        },
+        {
+            'name': 'Teplé nápoje',
+            'subsubcategories': [{'id': 6, 'name': 'Teplé nápoje'}]
+        },
+    ]
 
-"""def produkty_podla_kategorie(request, kategorie_id):
-    kategoria = get_object_or_404(Kategorie, id=kategorie_id)
-    produkty = Product.objects.filter(category=kategoria, available=True)
-    return render(request, 'products/produkty_podla_kategorie.html', {
-        'kategoria': kategoria,
-        'produkty': produkty
-    })"""
+    ostatne_sub = [
+        {
+            'name': 'Domácnosť a hygiena',
+            'subsubcategories': [{'id': 7, 'name': 'Domácnosť a hygiena'}]
+        },
+        {
+            'name': 'Pre deti a domácich miláčikov',
+            'subsubcategories': [{'id': 8, 'name': 'Pre deti a domácich miláčikov'}]
+        },
+        {
+            'name': 'Drobný tovar a sezónne veci',
+            'subsubcategories': [{'id': 9, 'name': 'Drobný tovar a sezónne veci'}]
+        },
+    ]
 
-def produkty_podla_kategorie(request, subcategory_id):
-    podkategoria = get_object_or_404(Podkategoria, id=subcategory_id)
-    produkty = Produkt.objects.filter(podkategoria=podkategoria)
-    return render(request, 'products/produkty_podla_kategorie.html', {'produkty': produkty, 'podkategoria': podkategoria})
+    context = {
+        'jedlo_sub': jedlo_sub,
+        'pitie_sub': pitie_sub,
+        'ostatne_sub': ostatne_sub,
+    }
+
+    return render(request, 'products/kategorie_list.html', context)
+
+
+def produkty_podla_kategorie(request, category_id):
+
+    return render(request, 'products/produkty_podla_kategorie.html', {'category_id': category_id})
