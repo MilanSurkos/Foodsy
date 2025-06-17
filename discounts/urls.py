02b0610e5049c93233d18888393ee49a2a321a1c
git config --global user.name "Iveta Szaboova <git config --global user.email "ivetka.szaboova@gmail.com>
2025-06-17 23:27:45 +0200

    Revert "úprava šablón v produktoch"
    This reverts commit 9d0ea6d9cdaf991c2b8fd428ce5277f3692a77cf.

--- discounts/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'discounts'
-
-urlpatterns = [
-    path('', views.discount_list, name='slevy'),
-]
--- discounts/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from products.models import Product
-
-def discount_list(request):
-    products = Product.objects.filter(is_discounted=True, discount_price__isnull=False)
-    return render(request, 'products/product_list.html', {'products': products})
