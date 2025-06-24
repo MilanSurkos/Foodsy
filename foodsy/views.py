dce8cab82b67f4da51d39c83a7f44eb672c8e77e
git config --global user.name "Iveta Szaboova <git config --global user.email "ivetka.szaboova@gmail.com>
2025-06-24 23:07:40 +0200

    úprava cenových trhákov , doplnenie EUR v košíku a úprava tlačítka Přidat do košíku v detaile produktu

--- foodsy/views.py
+++ foodsy/views.py
@@ -4,4 +4,10 @@ from products.models import Product
 
 def home(request):
     featured_products = Product.objects.all()[:4]  # Just show first 4
-    return render(request, 'home.html', {'featured_products': featured_products})
+    discounts_products = Product.objects.filter(is_discounted=True, discount_price__isnull=False)
+    basket = request.session.get('basket', {})
+    return render(request, 'home.html', {
+        'featured_products': featured_products,
+        'discounts_products': discounts_products,
+        'basket': basket,
+    })
